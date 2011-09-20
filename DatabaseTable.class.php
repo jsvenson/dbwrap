f928aa24a4e7660899af2c6fe1788006153d6c14
jared <jared@codespin.com>
2011-09-20 10:32:39 -0600

    Moved __callStatic() method out of template and into root DatabaseTable class. Marked root class as abstract.

--- DatabaseTable.class.php
+++ DatabaseTable.class.php
@@ -1,11 +1,12 @@
 <?php
 
+require_once('Inflector.class.php');
 require_once('dbConstants.class.php');
 
 /**
 * DatabaseTable
 */
-class DatabaseTable {
+abstract class DatabaseTable {
 	var $_db         = null;
 	var $_tablename  = '';
 	
@@ -155,6 +156,88 @@ class DatabaseTable {
 		sort($columns);
 		
 		return $flat ? '`'.implode('`,`', $this->columns()).'`' : $columns;
+	}
+	
+	# supports find_by_ and find_all_by_ for any column
+	# second argument of $args only supports order by clause
+	public static function __callStatic($name, $args) {
+		if (substr($name, 0, 11) != 'find_all_by' && substr($name, 0, 7) != 'find_by')
+			throw new Exception ('Unknown method call: '.$name);
+		
+		$limit     = substr($name, 0, 7) == 'find_by' ? 1 : 0;
+		$column    = substr($name, $limit==1?8:12);
+		$col_types = array();
+		
+		$data_types = array(
+			'bigint' => 'i', 'binary' => 'i', 'blob' => 'b', 'char' => 's', 'date' => 's', 'datetime' => 's', 'decimal' => 'd',
+			'enum' => 's', 'int' => 'i', 'longblob' => 'b', 'mediumblob' => 'b', 'mediumtext' => 's', 'set' => 's',
+			'smallint' => 'i', 'text' => 's', 'time' => 's', 'timestamp' => 's', 'tinyint' => 'i', 'varchar' => 's'
+		);
+		
+		$mysqli = new MySQLi(
+			dbConstants::_dbserver,
+			dbConstants::_dbuser,
+			base64_decode(dbConstants::_dbpass),
+			dbConstants::_dbname
+		);
+		
+		$tablename = Inflector::pluralize(get_called_class());
+		
+		$query = 'select column_name, data_type from information_schema.columns where table_schema=\''.dbConstants::_dbname.'\' and table_name=\''.$tablename.'\'';
+		$stmt = $mysqli->prepare($query);
+		$stmt->execute();
+		$stmt->bind_result($column_name, $data_type);
+		while ($stmt->fetch()) {
+			$col_types[$column_name] = $data_types[$data_type];
+		}
+		$stmt->close();
+		
+		if (!isset($col_types[$column])) throw new Exception('Unknown column \''.$column.'\'');
+		
+		$col_keys = array_keys($col_types);
+		
+		$query = 'select `'.implode('`,`', $col_keys).'` from `'
+			. $tablename .'` where `' . $mysqli->real_escape_string($column) . '` = ?';
+		
+		if (count($args) > 1) {
+			$order = isset($args[1]['order']) ? $mysqli->real_escape_string($args[1]['order']) : 'created asc';
+			$query .= ' order by '.$order;
+		}
+		
+		if ($limit > 0) $query .= ' limit 1';
+		
+		if (($stmt = $mysqli->prepare($query)) === false) throw new Exception('Problem preparing records: '.$mysqli->error);
+		
+		$stmt->bind_param($col_types[$column], $args[0]);
+		
+		if (!$stmt->execute()) throw new Exception('Problem reading '.self::_tablename.': '.$stmt->error."\n$query");
+		
+		$bound_names = array();
+		for ($i=0; $i < count($col_keys); $i++) { 
+			$bound_name = $col_keys[$i];
+			$$bound_name = '';
+			$bound_names[] = &$$bound_name;
+		}
+		
+		if (call_user_func_array(array($stmt, 'bind_result'), $bound_names) === false)
+			throw new Exception('Problem reading '.self::_tablename.': '.$stmt->error);
+		
+		$records   = array();
+		$classname = get_called_class();
+		while ($stmt->fetch()) {
+			$j = new $classname;
+			
+			for ($i=0; $i < count($col_keys); $i++) { 
+				$j->$col_keys[$i] = $$col_keys[$i];
+			}
+			
+			$records[] = $j;
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+		return $limit == 0 ? $records : $records[0];
 	}
 }
 
