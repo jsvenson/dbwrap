39b7038882826d74355766bb7bae601b2e842905
Jared Svenson <jared@codespin.com>
2011-10-27 21:48:50 -0600

    Added add, remove, and exists methods to Collection class.

--- Collection.class.php
+++ Collection.class.php
@@ -27,6 +27,51 @@ class Collection {
 		
 		return count($filtered);
 	}
+	
+	public function add($conditions, $classname) {
+		$keys = array_keys($conditions);
+		$vals = array_values($conditions);
+		
+		$o = new $classname();
+		for ($i=0; $i < count($keys); $i++) { 
+			$o->$keys[$i] = $vals[$i];
+		}
+		$o->save();
+		
+		$this->objects[] = $o;
+	}
+	
+	public function remove($conditions) {
+		$keys = array_keys($conditions);
+		$vals = array_values($conditions);
+		
+		$this->objects = array_filter($this->objects, function($el) use ($keys, $vals) {
+			$keepit = false;
+			for ($i=0; $i < count($keys); $i++) { 
+				if ($el->$keys[$i] != $vals[$i]) $keepit = true;
+			}
+			
+			if (!$keepit) $el->delete();
+			
+			return $keepit;
+		});
+	}
+	
+	public function exists($conditions) {
+		$keys = array_keys($conditions);
+		$vals = array_values($conditions);
+		
+		$leftovers = array_filter($this->objects, function($el) use($keys, $vals) {
+			$keepit = true;
+			for ($i=0; $i < count($keys); $i++) { 
+				if ($el->$keys[$i] != $vals[$i]) $keepit = false;
+			}
+			
+			return $keepit;
+		});
+		
+		return count($leftovers) > 0;
+	}
 }
 
 ?>
