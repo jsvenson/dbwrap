805a638d1e9921671b25c88e255656ea9acc9330
jared <jared@codespin.com>
2012-01-11 11:51:16 -0700

    Added missing back ticks around table name for selecting, inserting, and deleting.

--- DatabaseTable.class.php
+++ DatabaseTable.class.php
@@ -58,7 +58,7 @@ abstract class DatabaseTable {
 	public function delete() {
 		$this->openConnection();
 		
-		if (!($stmt = $this->_db->prepare('delete from '.$this->_tablename.' where id = ?')))
+		if (!($stmt = $this->_db->prepare('delete from `'.$this->_tablename.'` where id = ?')))
 			throw new Exception('Problem preparing delete statement: '.$this->_db->error);
 		
 		if (!$stmt->bind_param('i', $this->id))
@@ -81,7 +81,7 @@ abstract class DatabaseTable {
 		
 		$columns = '`'.implode('`,`', $this->columns()).'`';
 		
-		if (!($stmt = $this->_db->prepare('select '.$columns.' from '.$this->_tablename.' where id = ?')))
+		if (!($stmt = $this->_db->prepare('select '.$columns.' from `'.$this->_tablename.'` where id = ?')))
 			throw new Exception('Problem preparing select statement: '.$this->_db->error);
 		$stmt->bind_param('i', $id);
 		
@@ -119,7 +119,7 @@ abstract class DatabaseTable {
 			$cols   = '`created`, `updated`, '.'`'.implode('`, `', $columns).'`';
 			$values = 'now(), now()'.str_repeat(', ?', count($columns));
 			
-			if (!($stmt = $this->_db->prepare('insert into '.$this->_tablename.' ('.$cols.') values ('.$values.')')))
+			if (!($stmt = $this->_db->prepare('insert into `'.$this->_tablename.'` ('.$cols.') values ('.$values.')')))
 				throw new Exception('Problem preparing insert statement: '.$this->_db->error);
 			
 			$params = array();
